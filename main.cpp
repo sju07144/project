6a6ec41a15b6d248816438d34a6ea799501abb5a
sju07144 <sju07144@naver.com>
2020-05-30 16:02:57 +0900

    main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ using namespace std;
 
 int main(void)
 {
-	srand((unsigned
+	srand((unsigned)time(NULL));
 	vector<int> v;
-	
+	for(int i = 0; i < 10; i++)
+		v.push_back(rand() % 100);
+
+	sort(v.begin(), v.end(), [](const int& a, const int& b) { return a < b; });
+	for_each(v.begin(), v.end(), [](int i) { cout << i << endl; });
+	return 0;
+}
